fe0ed98587cb7eb9a5d0adc077763c0fd24f497a
vinmmm <jvinmoore@gmail.com>
2017-05-16 12:09:19 -0400

    react-create-app on Heroku

--- src/Card.js
+++ src/Card.js
@@ -17,13 +17,17 @@ if (this.state.showDetails) { cardDetails = (
 </div> );
 };
 
-
+// Using the JavaScript ! (not) operator to toggle the Boolean property showDetails
+// also use the ternary form to add a className conditionally to the Card Title 
     return (
 <div className="card">
-<div className="card__title" onClick={ ()=>this.setState({showDetails: !this.state.showDetails})
+<div className={
+this.state.showDetails? "card__title card__title--is-open" : "card__title"
+} onClick={ ()=>this.setState({showDetails: !this.state.showDetails})
 }>{this.props.title}</div>
 {cardDetails} </div>
-); }
+); 
+}
 
 }
 export default Card;
